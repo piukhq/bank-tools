79820ee878cca8d8fbd871716f94cf923cc21107
MartfBink <mfranklin@bink.com>
2022-07-15 16:43:23 +0100

    metis

--- bank-stack/run-bank-stack.sh
+++ bank-stack/run-bank-stack.sh
@@ -2,9 +2,9 @@
 
 ################################################################################
 # runs the Hermes & Angelia services needed to get API 1 & 2 up & running
-# TODO: Metis & Pelops
+# TODO: Pelops
 # Requires:
-#     hermes & angelia code, postgres database etc etc 
+#     hermes, angelia & metis code, postgres database etc etc 
 #     tmux
 ################################################################################
 
@@ -16,7 +16,7 @@ TMUX_SESSION_NAME='bank_stack'
 tmux -2 new-session -d -s $TMUX_SESSION_NAME
 tmux new-window -t $TMUX_SESSION_NAME -n 'bank'
 
-for p in {0..4}; do
+for p in {0..7}; do
   tmux split-pane -v
   tmux select-layout tiled
 done
@@ -32,21 +32,31 @@ tmux select-pane -t 0 -T Hermes-runserver
 tmux send-keys -t 0 "echo '** Launching Hermes Server **'" C-m
 tmux send-keys -t 0 "cd $directory/hermes && pipenv run python manage.py runserver" C-m
 ##
-tmux select-pane -t 1 -T api-messaging
+tmux select-pane -t 1 -T Hermes-api-messaging
 tmux send-keys -t 1 "echo '** Launching Hermes api-messaging **'" C-m
 tmux send-keys -t 1 "cd $directory/hermes && pipenv run python api_messaging/run.py" C-m
 ##
-tmux select-pane -t 2 -T celery
+tmux select-pane -t 2 -T Celery
 tmux send-keys -t 2 "echo '** Launching Hermes celery **'" C-m
 tmux send-keys -t 2 "cd $directory/hermes && pipenv run celery -A hermes worker --loglevel=INFO --concurrency=1 --queues=ubiquity-async-midas,record-history -E" C-m
 ##
-tmux select-pane -t 3 -T celery-beat
+tmux select-pane -t 3 -T Celery-beat
 tmux send-keys -t 3 "echo '** Launching Hermes celery-beat **'" C-m
 tmux send-keys -t 3 "cd $directory/hermes && pipenv run celery -A hermes beat --loglevel=INFO" C-m
 
 ##
-tmux select-pane -t 4 -T angelia
+tmux select-pane -t 4 -T Angelia
 tmux send-keys -t 4 "echo '** Launching Angelia **'" C-m
 tmux send-keys -t 4 "cd $directory/angelia && pipenv run python commands.py run-api-server" C-m
+
+##
+tmux select-pane -t 5 -T Metis-runserver
+tmux send-keys -t 5 "echo '** Launching Metis Runserver **'" C-m
+tmux send-keys -t 5 "cd $directory/metis && pipenv run flask run -p 8095" C-m
+
+##
+tmux select-pane -t 6 -T Metis-celery
+tmux send-keys -t 6 "echo '** Launching Metis celery **'" C-m
+tmux send-keys -t 6 "cd $directory/metis && pipenv run celery -A app.tasks worker --loglevel=INFO --concurrency=1 " C-m
 
 tmux attach-session -t 'bank'
